8d8db449686b7271a7b514d72006af6176fd5acd
johannorens <johannorens009@gmail.com>
2026-06-07 21:51:02 +0000

    CategorieController et modif updatecontroller

--- Akwab-Api/routes/api.php
+++ Akwab-Api/routes/api.php
@@ -1,11 +1,17 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategorieController;
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/register/organisateur', [AuthController::class, 'registerOrganisateur']);
 Route::post('/register/admin', [AuthController::class, 'registerAdmin']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('categories', CategorieController::class);
+});
