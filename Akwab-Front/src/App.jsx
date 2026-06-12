6e7fb890dbe29f89d89767dc6b14fd55b2d20c0f
johannorens <johannorens009@gmail.com>
2026-06-12 10:51:26 +0000

    authentifications pages

--- Akwab-Front/src/App.jsx
+++ Akwab-Front/src/App.jsx
@@ -1,13 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingPage from "./LoadingPage";
 import Login from "./Authentification/Login";
+import Register from "./Authentification/Register";
+import ResetPassword from "./Authentification/ResetPassword";
+import ForgotPassword from "./Authentification/ForgotPassword";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* public route */}
         <Route path="/" element={<LoadingPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
