a5ba805f930efef3b00d228d8b44985acfa6ab99
johannorens <johannorens009@gmail.com>
2026-07-02 06:00:31 +0000

    Add production front domain to CORS allowed origins

--- Akwab-Api/docker/start.sh
+++ Akwab-Api/docker/start.sh
@@ -5,27 +5,21 @@ echo ">>> START.SH IS RUNNING <<<"
 
 export PORT="${PORT:-8080}"
 
-echo ">>> PORT IS: $PORT <<<"
-
 envsubst '${PORT}' < /etc/nginx/http.d/default.conf.template > /etc/nginx/http.d/default.conf
-
-echo ">>> NGINX CONFIG GENERATED <<<"
 
 cd /var/www/api
 
 php artisan config:clear
-echo ">>> CONFIG CLEARED <<<"
 php artisan route:clear
-echo ">>> ROUTE CLEARED <<<"
 php artisan view:clear
-echo ">>> VIEW CLEARED <<<"
+
+php artisan migrate --force
+
+php artisan db:seed --class=SuperAdminSeeder --force
 
 php artisan config:cache
-echo ">>> CONFIG CACHED <<<"
 php artisan route:cache
-echo ">>> ROUTE CACHED <<<"
 php artisan view:cache
-echo ">>> VIEW CACHED <<<"
 
 echo ">>> LAUNCHING SUPERVISORD <<<"
 exec supervisord -c /etc/supervisord.conf
