a40d755a3fa4ab9861f4fe3124f6e8b8d60d1b4c
Aloneeveryday2 <axelteky333@gmail.com>
2026-07-16 10:44:37 +0000

    chore: ajout de Sentry pour la supervision des erreurs et le monitoring de performance

--- Akwab-Api/bootstrap/app.php
+++ Akwab-Api/bootstrap/app.php
@@ -4,6 +4,8 @@ use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
+
 
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -20,5 +22,5 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        Integration::handles($exceptions);
     })->create();
